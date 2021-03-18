9d9a362e78ea4b69a562221791f80c177752e28a
Thereza Bandoli <tcbandolilegg@gmail.com>
2021-03-18 20:40:46 -0300

    Acertando o user.js da Nory

--- frontEnd/js/cadastroUsuario.js
+++ frontEnd/js/cadastroUsuario.js
@@ -4,37 +4,37 @@ const botaoSalvarUserCadastro = document.querySelector("#botaoSalvarUserCadastro
 const userCadastro = document.querySelector("#userCadastro")
 const nomeCadastro = document.querySelector("#nomeCadastro")
 const emailCadastro = document.querySelector("#emailCadastro")
-const = document.querySelector("#senhaCadastro")
-const = document.querySelector("#senhaCadastroConfirma")
+const senhaCadastro = document.querySelector("#senhaCadastro")
+const senhaCadastroConfirma = document.querySelector("#senhaCadastroConfirma")
 
 
 botaoSalvarUserCadastro.addEventListener('click', (event) => {
-  const emailGlobal = userCadastro.value
-  const senhaGlobal = senhaCadastro.value
+  const userCadastroGlobal = userCadastro.value
+  const senhaCadastroGlobal = senhaCadastro.value
+  const botaoSalvarUserCadastroGlobal = botaoSalvarUserCadastro.value
+  const userCadastroGlobal = userCadastro.value
+  const nomeCadastroGlobal = nomeCadastro.value
+  const emailCadastroGlobal = emailCadastro.value
+  const senhaCadastroGlobal = senhaCadastro.value
+  const senhaCadastroConfirmaGlobal = senhaCadastroConfirma.value
   const idUserGlobal = 0
-  verificaUser(emailGlobal, senhaGlobal, idUserGlobal)
+
+  verificaUser(userCadastroGlobal, emailCadastroGlobal)
 })
 
-
-
-
-
-
-
-function verificaUser(emailGlobal, senhaGlobal, idUserGlobal) {
+function verificaUser(userCadastroGlobal, emailCadastroGlobal) {
   const fimArquivo = json.length
-  console.log(fimArquivo)
   let contador = 0
   //mudar o forEach pelo filter
   // let novoUserTemp = {}
   // novoUserTemp.email = email
   // novoUserTemp.senha = senha
-
   // const novoUser = JSON.stringify(novoUserTemp)
-
   // fetch(localhost: 3000 / login / emailGlobal / senhaGlobal, { method: "GET" })
+
   json.forEach(element => {
-    if (element.email == emailGlobal && element.senha == senhaGlobal) {
+    if (element.email == emailCadastroGlobal || element.senha == userCadastroGlobal) {
+      alert('Usuário já existe')
       // como  enviar o email e o id do user???
       idUserGlobal = element.id
       localStorage.setItem("idUserGlobal", idUserGlobal);
